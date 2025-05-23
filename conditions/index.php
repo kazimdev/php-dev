5a5097f5dfddb456e3a68cd5a64d0c8ae8964a04
Mak Alamin <makalamin012@gmail.com>
2025-05-23 21:43:43 +0600

    condition

---
+++ conditions/index.php
@@ -0,0 +1,82 @@
+<?php
+
+// $season = "Winter";
+
+// if($season == "Summer"){
+//    echo "Aam pakche. Ami Aam khabo.";
+
+// } else {
+//     echo "Aam pake nai. Khawa jabe na.";
+// }
+
+
+// $student_name = "Mosharof";
+
+// $student_number = 80;
+
+
+
+
+
+// if($student_number >= 80){
+//     echo $student_name . " got A+ grade";
+
+// } elseif($student_number >= 70 AND $student_number < 80 ){
+//     echo $student_name . " got A grade";
+
+// } elseif($student_number >= 60 AND $student_number < 70){
+//     echo $student_name . " got B grade";
+
+// } elseif($student_number >= 50 AND $student_number < 60){
+//     echo $student_name . " got C grade";
+
+// } elseif($student_number >= 40 AND $student_number < 50){
+//     echo $student_name . " got D grade";
+
+// } else {
+//     echo $student_name . " got F grade";
+// }
+
+
+// $_name = "Sagor";
+
+// $_city = "Dhaka";
+// $_age = 99;
+// $_country = "France";
+
+// if( ($_city == "Dhaka" || $_age > 17) && $_country == "BD"){
+//     echo "You are welcome";
+
+// } else {
+//     echo "You are restricted.";
+// }
+
+
+
+// $age = 10;
+
+// $isAdult = ($age >= 18) ? "Adult" : "Not Adult";
+
+// echo $isAdult;
+
+
+
+$color = "asdfdf";
+
+switch($color){
+    case 'red':
+        echo "Blood is Red";
+        break;
+
+    case 'blue':
+        echo "Sky is Blue";
+        break;
+    
+    case "white":
+        echo "Milk is white";
+        break;
+
+    default:
+        echo "Transparent";
+
+}
